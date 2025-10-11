b86a566e80518f316e29f5b63e67f1217ca66667
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-11 14:24:36 +0530

    Update settings.py

--- QGen/settings.py
+++ QGen/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
