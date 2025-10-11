cb9be3a0161ce59c80183c8378c72dca31af309c
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-11 14:22:02 +0530

    Update settings.py

--- QGen/settings.py
+++ QGen/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project li
+# Build paths inside the project l
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
