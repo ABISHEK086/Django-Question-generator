5c60b786d20ea7ed3d18af44f2cb1c921142c3a6
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-24 20:22:53 +0530

    Update admin.py

--- QPaperGeneration/admin.py
+++ QPaperGeneration/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin                 
 from QPaperGeneration.models import User, QPattern
 
-admin.site.register(User)   
+admin.site.register(User)    
 admin.site.register(QPattern)         
