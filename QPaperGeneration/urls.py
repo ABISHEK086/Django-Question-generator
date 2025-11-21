800b1d133787fbedb6b63a06cfac6f92e9e4226d
ABISHEK ANTONY <abishek8673@gmail.com>
2025-11-21 20:08:20 +0530

    Update urls.py

--- QPaperGeneration/urls.py
+++ QPaperGeneration/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views 
 
-urlpatterns = [    
+urlpatterns = [     
     # Main dashboard and authentication
     path("", views.dashboard, name="dashboard"),
     path("index", views.index, name="index"),
