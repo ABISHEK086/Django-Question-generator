94b1ab12e750c112e76e4fe541be37fe4d969dc9
ABISHEK ANTONY <abishek8673@gmail.com>
2026-01-12 17:48:14 +0530

    Update models.py

--- QPaperGeneration/models.py
+++ QPaperGeneration/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     ROLE_CHOICES = [           
         ('student', 'Student'),                
         ('staff', 'Staff'),                
-        ('admin', 'Admin'),     
+        ('admin', 'Admin'),      
     ]  
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
     
