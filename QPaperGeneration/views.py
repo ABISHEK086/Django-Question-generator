f277c6a1748f14cd8a04b15ddc5e5cb3ebea5e63
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-02 15:11:57 +0530

    Update views.py

--- QPaperGeneration/views.py
+++ QPaperGeneration/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse     
 from django.core.paginator import Paginator   
 from django.contrib.auth.decorators import login_required      
-from django.contrib import messages   
+from django.contrib import messages    
 from reportlab.lib.pagesizes import A4  
 from reportlab.pdfgen import canvas  
 from reportlab.lib.utils import simpleSplit    
